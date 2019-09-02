a83122a13bd13e2eb4de8df3879c7311d4411e29
henry kirya <henry.kirya@gmail.com>
2019-09-02 08:56:51 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [ 
+{ text: "Hi there!", user: "Amando", added: new Date() },
+{ text: "Hello World!", user: "Charles", added: new Date() },
+{ text: "kyi kati", user: "mwana", added: new Date() },
+{ text: "tuliyo basa", user: "henuule", added: new Date() },
+{ text: "habari yako", user: "mjaama", added: new Date() }
+]; 
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', function(req, res, next) {
+	let uuser = req.body.user;
+	let mmessg = req.body.messg;
+	messages.push({text: mmessg, user: uuser, added: new Date()})
+	res.redirect('/')
+});
+
+
+module.exports = router;
+
+
+
+
+
